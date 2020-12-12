b05e9216f79eb808c7a9db3e634c04c47a304cdb
h1202019034@hiratagakuen.onmicrosoft.com <h1202019034@hiratagakuen.onmicrosoft.com>
2020-12-12 13:52:58 +0900

    チャイナタウン会話

--- Project2/Project2/ObjChinaMob.cpp
+++ Project2/Project2/ObjChinaMob.cpp
@@ -44,25 +44,62 @@ void CObjChinaMob::Action()
 	CObjChinaTownBoss* chinatownboss = (CObjChinaTownBoss*)Objs::GetObj(OBJ_CHINA_TOWN_BOSS);//チャイナタウンボス
 	CObjChinaTown_b* chinatown_b = (CObjChinaTown_b*)Objs::GetObj(OBJ_CHINA_TOWN_B);//チャイナタウンのB
 
-	int minute;//分
-	int second;//秒
-
-	m_time++;
-
-	second = (m_time / 60) % 60;//秒
-	minute = (m_time / 60) / 60;//分
-
 	
-
-
-
-
 	//チャイナタウンのモブ
 	if (chinatown != nullptr)
 	{
 		if (hero->GetBT() == 99)
 		{
 			mob_flag = 4;
+			if (Input::GetVKey(VK_RETURN) == true) {
+
+				if (m_key_control == true)
+				{
+					if (key_flag == 1)
+					{
+						m_sp = 1;
+						sp_flag = true;
+					}
+
+					if (key_flag == 2)
+					{
+						m_sp = 2;
+						sp_flag = true;
+					}
+					if ((key_flag == 3))
+					{
+
+						m_sp = 3;
+						sp_flag = true;
+					}
+					if ((key_flag == 4))
+					{
+						m_sp = 4;
+						sp_flag = true;
+					}
+					if ((key_flag == 5))
+					{
+						m_sp = 5;
+						sp_flag = true;
+					}
+					if ((key_flag == 6))
+					{
+						m_sp = 6;
+						sp_flag = true;
+					}
+					m_key_control = false;
+				}
+
+			}
+			else
+			{
+				m_key_control = true;
+			}
+		}
+
+		if (hero->GetBT() ==34)//車
+		{
+			mob_flag =1;
 			if (Input::GetVKey(VK_RETURN) == true) {
 
 				if (m_key_control == true)
@@ -387,7 +424,7 @@ void CObjChinaMob::Draw()
 			key_flag = 2;
 			fin.close();//ファイルを閉じる
 		}
-		if (m_sp == 2)//エンターキーを2回押したとき
+		if (m_sp == 2 && mob_flag == 4)//エンターキーを2回押したとき
 		{
 			sp_flag == true;
 
@@ -405,7 +442,7 @@ void CObjChinaMob::Draw()
 			key_flag = 3;
 			fin.close();//ファイルを閉じる
 		}
-		if (m_sp == 3)//エンターキーを3回押したとき
+		if (m_sp == 3 && mob_flag == 4)//エンターキーを3回押したとき
 		{
 			sp_flag = false;
 
@@ -413,10 +450,55 @@ void CObjChinaMob::Draw()
 
 			key_flag = 1;
 		}
-
-
-		
 	}
+	if (chinatown != nullptr)//車
+	{
+		if (m_sp == 1 && mob_flag == 1)//エンターキーを一回押したとき
+		{
+			sp_flag == true;
+
+			ifstream fin("チャイナタウン会話.txt", ios::in);//テキストデータをを読み込み
+			char str1[64];//ただの配列
+			wchar_t wstr1[64];
+			fin.seekg(100, ios::cur);//0バイト数進める
+			fin >> str1;//str1にテキストを入れる
+
+			sprintf_s(str1, "%s", str1);//出力
+			MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str1, 64, wstr1, 64);//文字をユニコードに変換する
+			Font::StrDraw(wstr1, 50.0f, 500, 30, c);// X  Y  大きさ     
+
+
+			key_flag = 2;
+			fin.close();//ファイルを閉じる
+		}
+		if (m_sp == 2 && mob_flag == 1)//エンターキーを2回押したとき
+		{
+			sp_flag == true;
+
+			ifstream fin("チャイナタウン会話.txt", ios::in);//テキストデータをを読み込み
+			char str1[64];//ただの配列
+			wchar_t wstr1[64];
+			fin.seekg(114, ios::cur);//0バイト数進める
+			fin >> str1;//str1にテキストを入れる
+
+			sprintf_s(str1, "%s", str1);//出力
+			MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str1, 64, wstr1, 64);//文字をユニコードに変換する
+			Font::StrDraw(wstr1, 50.0f, 500, 25, c);// X  Y  大きさ     
+
+
+			key_flag = 3;
+			fin.close();//ファイルを閉じる
+		}
+		if (m_sp == 3 && mob_flag == 1)//エンターキーを3回押したとき
+		{
+			sp_flag = false;
+
+			Font::StrDraw(L"", 50.0f, 500, 25, c);// X  Y  大きさ     
+
+			key_flag = 1;
+		}
+	}
+
 
 	if (chinatown != nullptr) 
 	{

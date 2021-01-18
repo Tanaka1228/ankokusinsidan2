459ac01d06e5cdf232ecd0a87fa90010bcc0a581
泰壱　河村 <h1202019034@hiratagakuen.onmicrosoft.com>
2021-01-18 16:21:58 +0900

    病院の一階の敵

--- Project2/Project2/CObjChinaAtkEnemy.cpp
+++ Project2/Project2/CObjChinaAtkEnemy.cpp
@@ -50,6 +50,11 @@ void CObjChinaAtkEnemy::Init()
 		//当たり判定用HitBoxを作成
 		Hits::SetHitBox(this, m_x, m_y, 64, 64, ELEMENT_ENEMY, OBJ_CHINA_ATK_ENEMY, 1);
 	}
+	if (m_id == 5)
+	{
+		//当たり判定用HitBoxを作成
+		Hits::SetHitBox(this, m_x, m_y, 64, 64, ELEMENT_ENEMY, OBJ_CHINA_ATK_ENEMY, 1);
+	}
 }
 
 //アクション
@@ -63,6 +68,7 @@ void CObjChinaAtkEnemy::Action()
 	CObjChinaTown_b* chinatownb = (CObjChinaTown_b*)Objs::GetObj(OBJ_CHINA_TOWN_B);
 	//チャイナタウンcの情報
 	CObjChinaTown_c* chinatownc = (CObjChinaTown_c*)Objs::GetObj(OBJ_CHINA_TOWN_C);
+	CObjHospital* hospital = (CObjHospital*)Objs::GetObj(OBJ_HOSPITAL);//病院の一階
 
 	//チャイナタウン雑魚
 	if (m_id == 1) {
@@ -215,6 +221,44 @@ void CObjChinaAtkEnemy::Action()
 			}
 		}
 	}
+
+	//病院一階 の雑魚
+	if (m_id == 5)
+	{
+		m_time++;//1加算
+
+		if (m_time > 100)//弾丸を発射する間隔
+		{
+			m_time = 0;
+
+			//弾丸敵機オブジェクト
+			CObjBulletEnemy* obj_bullet_enemy = new CObjBulletEnemy(m_x, m_y, 6);
+			Objs::InsertObj(obj_bullet_enemy, OBJ_BULLET_ENEMY, 4);
+		}
+
+
+		//敵機が完全に領域外に出たら敵機を破棄する
+		bool check = CheckWindow(m_x, m_y, -32.0f, -32.0f, 3000.0f, 3000.0f);
+		if (check == false)
+		{
+			this->SetStatus(false);//自身に削除命令
+			Hits::DeleteHitBox(this);
+
+			return;
+		}
+
+		//HitBoxの内容を更新
+		CHitBox* hit = Hits::GetHitBox(this);
+		hit->SetPos(m_x + hospital->GetScroll(), m_y + hospital->GetScroll2());
+
+		//弾丸と接触してるかどうか調べる
+		if (hit->CheckObjNameHit(OBJ_BULLET) != nullptr)
+		{
+			this->SetStatus(false);
+			Hits::DeleteHitBox(this);
+			Audio::Start(5);
+		}
+	}
 }
 
 //ドロー
@@ -228,6 +272,7 @@ void CObjChinaAtkEnemy::Draw()
 	CObjChinaTown_b* chinatownb = (CObjChinaTown_b*)Objs::GetObj(OBJ_CHINA_TOWN_B);
 	//チャイナタウンcの情報
 	CObjChinaTown_c* chinatownc = (CObjChinaTown_c*)Objs::GetObj(OBJ_CHINA_TOWN_C);
+	CObjHospital* hospital = (CObjHospital*)Objs::GetObj(OBJ_HOSPITAL);//病院の一階
 
 	//描画カラー情報　R=RED G=Green B=Blue A=alpha(透過情報)
 	float c[4] = { 0.5f,2.5f,1.5f,1.0f };
@@ -292,6 +337,20 @@ void CObjChinaAtkEnemy::Draw()
 
 			//1番目に登録したグラフィックをstc・dst・cの情報を元に描画
 			Draw::Draw(1, &src, &dst, c, 0.0f);
+		}
+	}
+	if (hospital != nullptr)
+	{
+		if (m_id == 5)
+		{
+			//表示位置の設定
+			dst.m_top = 0.0f + m_y + hospital->GetScroll2();
+			dst.m_left = 32.0f + 32.0f + m_x + hospital->GetScroll();
+			dst.m_right = 0.0f + m_x + hospital->GetScroll();
+			dst.m_bottom = 32.0f + 32.0f + m_y + hospital->GetScroll2();
+
+			//1番目に登録したグラフィックをstc・dst・cの情報を元に描画
+			Draw::Draw(11, &src, &dst, c, 0.0f);
 		}
 	}
 }
